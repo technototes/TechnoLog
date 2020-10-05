afebe620818886203c96bdf3f2b695c566f524a6
1198159 <jeffastedman@gmail.com>
2020-10-04 20:28:44 -0700

    pog log

--- RobotLibrary/src/main/java/com/technototes/library/structure/TeleOpCommandOpMode.java
+++
@@ -1,8 +0,0 @@
-package com.technototes.library.structure;
-
-import com.technototes.library.control.gamepad.CommandGamepad;
-
-public abstract class TeleOpCommandOpMode extends CommandOpMode {
-    public CommandGamepad driverGamepad = new CommandGamepad(gamepad1);
-    public CommandGamepad codriverGamepad = new CommandGamepad(gamepad2);
-}
--- RobotLibrary/src/main/java/com/technototes/library/subsystem/drivebase/EncodedSwerveDrivebaseSubsystem.java
+++
@@ -1,9 +0,0 @@
-package com.technototes.library.subsystem.drivebase;
-
-import com.technototes.library.hardware.motor.EncodedMotor;
-
-public class EncodedSwerveDrivebaseSubsystem extends SwerveDrivebaseSubsystem<EncodedMotor> {
-    public EncodedSwerveDrivebaseSubsystem(EncodedMotor... d) {
-        super(d);
-    }
-}
